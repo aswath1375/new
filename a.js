a96515cde28e19f54e4218ac86b4fd9f5ba3c368
ASWATH SOORYA <suryaaswath640@gmail.com>
2025-10-08 10:55:32 +0530

    Update a.js

--- a.js
+++ a.js
@@ -60,3 +60,12 @@ function validateForm(event) {
 
   return true;
 }
+
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const sidebar = document.getElementById('sidebar');
+
+  menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+  });
+
